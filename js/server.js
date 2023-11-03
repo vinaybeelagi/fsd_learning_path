1c5c7aa0402602a69a5189d76b28e42169d6ae14
SvdNeo <sanjay.d@neoito.com>
2023-11-03 14:09:44 +0530

    querryselector

--- js/server.js
+++ js/server.js
@@ -27,10 +27,17 @@
 // here we get the pop up alert giving us the number of h2 headings in our HTML page
 
 // getElementsByClassNAme
-let menu = document.getElementById("menu");
-let items = menu.getElementsByClassName('items'); `--> This will provide us items in form of HTML collection `
-let itemsArray = [...items];`---> this will convert HTML collection to array`
+// let menu = document.getElementById("menu");
+// let items = menu.getElementsByClassName('items'); `--> This will provide us items in form of HTML collection `
+// let itemsArray = [...items];`---> this will convert HTML collection to array`
 
-let data = itemsArray.map(i => i.textContent);`---> here we can use map to iterate over all the text elements `
+// let data = itemsArray.map(i => i.textContent);`---> here we can use map to iterate over all the text elements `
 
-console.log(data);
+// console.log(data);
+
+// querryselector method
+
+const firstEle = document.querySelector(".items");
+console.log(firstEle.textContent);`---> here we get the first element from the list as it matches all the items but returns only first item`
+
+
