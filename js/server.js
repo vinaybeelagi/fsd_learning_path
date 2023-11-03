f92a9db1cfa697df2e3253e5eed33f4034f0ff6a
SvdNeo <sanjay.d@neoito.com>
2023-11-03 10:32:11 +0530

    getElementById

--- js/server.js
+++ js/server.js
@@ -2,14 +2,15 @@
 const p = document.getElementById("message");
 console.log(p) 
 // we get output as "A paragraph"
-let btn = document.getElementById("btn");
+let btn = document.getElementById('btnRate');
+        let output = document.getElementById('output');
 
-let output = document.getElementById("output");
-btn.addEventListener("click",()=>{
-    let rates = document.getElementsByName("rate");
-    rates.forEach((rate)=>{
-        if(rate.checked){
-            output.innerText = `You selected : ${rate.value}`;
-        }
-    });
-})
+        btn.addEventListener('click', () => {
+            let rates = document.getElementsByName('rate');
+            rates.forEach((rate) => {
+                if (rate.checked) {
+                    output.innerText = `You selected: ${rate.value}`;
+                }
+            });
+
+        });
