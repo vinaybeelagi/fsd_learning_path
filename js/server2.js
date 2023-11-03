d2b2cb2cc32f3ca2c6b234dadccd33b800fc38ce
vinaybeelagi <vinaykumar.b@neoito.com>
2023-11-03 17:25:01 +0530

    JS Get Child Elements

--- js/server2.js
+++ js/server2.js
@@ -1,4 +1,8 @@
 // JavaScript parentNode
-let paper = document.querySelector('.paper');
-console.log(paper.parentNode);  
-// ouput:<div id="main"> its contents, 'div' is parent node.
+// let paper = document.querySelector('.paper');
+// console.log(paper.parentNode);  
+// // ouput:<div id="main"> its contents, 'div' is parent node.
+
+// // Get the first child element
+let content = document.getElementById('menu');
+console.log(content.firstChild.nodeName);
