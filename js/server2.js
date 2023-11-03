dcfa4eac37da956b62351fd79c3e9d5fee46ed1a
vinaybeelagi <vinaykumar.b@neoito.com>
2023-11-03 18:26:44 +0530

    get all the next siblings of an element

--- js/server2.js
+++ js/server2.js
@@ -3,8 +3,23 @@
 // console.log(paper.parentNode);  
 // // ouput:<div id="main"> its contents, 'div' is parent node.
 
-// // Get the first child element
-let content = document.getElementById('menu');
-// console.log(content.firstChild.nodeName);
-// console.log(content.lastElementChild);  `-->Get the last child element`
-console.log(content.children) `-->Get all child elements`
+// // // Get the first child element
+// let content = document.getElementById('menu');
+// // console.log(content.firstChild.nodeName);
+// // console.log(content.lastElementChild);  `-->Get the last child element`
+// console.log(content.children) `-->Get all child elements`
+
+// // Get the next siblings
+// let current = document.querySelector('.current');
+// let nextSibling = current.nextElementSibling;
+// // To get the next sibling of an element, you use the nextElementSibling attribute
+// console.log(nextSibling);
+
+// get all the next siblings of an element
+let current = document.querySelector('.current');
+let nextSibling = current.nextElementSibling;
+
+while(nextSibling) {
+    console.log(nextSibling);
+    nextSibling = nextSibling.nextElementSibling;
+}
