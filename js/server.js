484d5a2d9cf00091bce5569bab3b5339d7afd770
vinaybeelagi <vinaykumar.b@neoito.com>
2023-11-03 10:30:22 +0530

    Dom ElementsByName

--- js/server.js
+++ js/server.js
@@ -1,1 +1,13 @@
-console.log("Welcome to Neoito")
+// console.log("Welcome to Neoito")
+let btn = document.getElementById('btnRate');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+    let rates = document.getElementsByName('rate');
+    rates.forEach((rate) => {
+        if (rate.checked) {
+            output.innerText = `You selected: ${rate.value}`;
+        }
+    });
+
+});
