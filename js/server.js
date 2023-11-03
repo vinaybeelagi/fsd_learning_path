18424612a081e55ee9f169f9ae1554c8c0c64ddd
SvdNeo <sanjay.d@neoito.com>
2023-11-03 11:26:35 +0530

    getElementByTagNAme

--- js/server.js
+++ js/server.js
@@ -2,6 +2,7 @@
 const p = document.getElementById("message");
 console.log(p) 
 // we get output as "A paragraph"
+// getElementByName
 let btn = document.getElementById('btnRate');
 // here button is connected by id "btnRate"
         let output = document.getElementById('output');
@@ -16,3 +17,11 @@ let btn = document.getElementById('btnRate');
 
         });
         // here we  use addeventlistener to initiate the event of button press and  get a statement depicting the rating we select
+
+        // getElementByTagName
+        let btn2 = document.getElementById("btn");
+        btn2.addEventListener('click',()=>{
+            let heading = document.getElementsByTagName("h2")
+            alert(`The total number of H2 headings are : ${heading.length}`)
+        })
+// here we get the pop up alert giving us the number of h2 headings in our HTML page
