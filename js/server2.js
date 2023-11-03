308cfcd66d114ac0d7876ea886f8e9d22f6d1bf3
SvdNeo <sanjay.d@neoito.com>
2023-11-03 17:41:29 +0530

    childnode

--- js/server2.js
+++ js/server2.js
@@ -1,2 +1,10 @@
-let doc = document.querySelector(".parent");
-console.log(doc.parentNode); `---> this will provide us complete div element as, nodetype is 'div'  and it is parent node `
+// parentNode
+// let doc = document.querySelector(".parent");
+// console.log(doc.parentNode.childNodes); `---> this will provide us complete div element as, nodetype is 'div'  and it is parent node `
+
+// Childnode
+let teams = document.getElementById("main");
+// console.log(teams.firstChild) `---> this provides us text as it depicts empty space between ul and li element`
+// console.log(teams.firstElementChild)`--> so in order to access first element we need to use firstelementchild `
+ console.log(teams.lastElementChild);`--> so in order to access last element we need to use lastelementchild `
+// console.log(teams.children);`--> so in order to access all the child we need to use children `
