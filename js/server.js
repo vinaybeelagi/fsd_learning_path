c9a7a4a3de1c6c2fc62dc5cebf9ce1310265bf3f
SvdNeo <sanjay.d@neoito.com>
2023-11-03 10:50:55 +0530

    comments on js

--- js/server.js
+++ js/server.js
@@ -3,8 +3,9 @@ const p = document.getElementById("message");
 console.log(p) 
 // we get output as "A paragraph"
 let btn = document.getElementById('btnRate');
+// here button is connected by id "btnRate"
         let output = document.getElementById('output');
-
+// here we use getElementById to display the output 
         btn.addEventListener('click', () => {
             let rates = document.getElementsByName('rate');
             rates.forEach((rate) => {
@@ -14,3 +15,4 @@ let btn = document.getElementById('btnRate');
             });
 
         });
+        // here we  use addeventlistener to initiate the event of button press and  get a statement depicting the rating we select
