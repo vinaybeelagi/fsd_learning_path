b2970d6f2bdf9562bdae2b335e1aea46b7878b3c
vinaybeelagi <vinaykumar.b@neoito.com>
2023-11-06 13:13:58 +0530

    Create ul,li and append child

--- js/server3.js
+++ js/server3.js
@@ -14,3 +14,29 @@ let h2 = document.createElement('h2');
 h2.textContent = 'Add h2 element to div';
 div.appendChild(h2);
 document.body.appendChild(div);
+
+// Creating new list items (li) example
+// let li = document.createElement('li');
+// li.textContent = 'products';
+// menu.appendChild(li);
+
+// li=document.createElement('li')
+// li.textContent = 'About Us';
+// menu.appendChild(li);
+
+// const menu = document.querySelector('#menu');
+// menu.appendChild(li);
+
+
+// Creating new list items (ul,li) example
+let ul = document.createElement('ul');
+ul.id = "cars"
+let li1 = document.createElement('li');
+li1.textContent='TATA'
+ul.appendChild(li1)
+let li2 = document.createElement('li');
+li2.textContent='Ford'
+ul.appendChild(li2)
+document.body.appendChild(ul);
+
+
